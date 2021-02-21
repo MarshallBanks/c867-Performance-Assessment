2eac64d522b1832487b86cc97b1dd33938a3f474
MegaMarsh <marshallmelonhead@hotmail.com>
2021-02-21 13:54:43 -0800

    Updated studentData Table with personal information for student A5.

--- Source.cpp
+++ Source.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-const string studentData[] =
+/*const string studentData[] =
 
 {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", 
 "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", 
-"A5,[firstname],[lastname],[emailaddress],[age], [numberofdaystocomplete3courses],SOFTWARE"
+"A5,Marshall,Banks,marshallmelonhead@hotmail.com,29,60,65,70,SOFTWARE"*/
 
 int main() {
 	cout << "Hello World" << endl;
 	cout << "A single change." << endl;
-
+	//cout << studentData;
 	return 0;
 }
