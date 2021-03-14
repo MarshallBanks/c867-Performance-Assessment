6e2604d71aa1c04597224068eb159496c17c54f8
MegaMarsh <marshallmelonhead@hotmail.com>
2021-03-14 13:51:23 -0700

    Tried and failed to get a parralel string for the enum DegreeProgram in the degree.h file. Reverted and saved not sure what else was changed.

--- degree.h
+++ degree.h
@@ -3,5 +3,3 @@
 using namespace std;
 #pragma once
 enum DegreeProgram {SECURITY, NETWORK, SOFTWARE};
-//string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" }; 
-//FIXME: Continue working on the days in cours array before finishing getting this to work.
