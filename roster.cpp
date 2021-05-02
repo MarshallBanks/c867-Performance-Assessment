aa2b450b68bcd2b7b19751939d6022e1be012f2d
MegaMarsh <marshallmelonhead@hotmail.com>
2021-05-02 14:28:13 -0700

    Started working on the roster.parse() function using David bishop videos.

--- roster.cpp
+++ roster.cpp
@@ -1,3 +1,12 @@
 #include "roster.h"
 
 
+void Roster::parse() {
+	int i = 0;
+	for (i = 0; i < numStudents; ++i) {
+		string currentString = studentData[i];
+		vector<string> result;
+		stringstream S_stream()
+	}
+}
+
