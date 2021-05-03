cf82bb90cef27ac01989820995d9d3bcbf7cfefa
MegaMarsh <marshallmelonhead@hotmail.com>
2021-05-02 17:26:59 -0700

    Removed code from main() having to do with iterative testing. Moved studentData[] back to main.cpp

--- main.cpp
+++ main.cpp
@@ -10,43 +10,21 @@ using namespace std;
 
 int main() {
 
-	
+	{
+		"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+		"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+		"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+		"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+		"A5,Marshall,Banks,marshallmelonhead@hotmail.com,29,51,59,60,SOFTWARE";
+	}
 
 	cout << "C867-Scripting and Programming: Applications" << endl;
 	cout << "Language: C++" << endl;
 	cout << "Student ID#: 000616668" << endl;
 	cout << "Name: Marshall Banks\n" << endl;
 
-	Student marshall;
 
-	int marshallDays[3] = { 51, 59, 60 }; //FIXME: For iterative testing
-
-	marshall.SetStudentId("A5"); //FIXME: For iterative testing
-	marshall.SetFirstName("Marshall"); //FIXME: For iterative testing
-	marshall.SetLastName("Banks"); //FIXME: For iterative testing
-	marshall.SetEmailAddress("marshallmelonhead@hotmail.com"); //FIXME: For iterative testing
-	marshall.SetAge(29); //FIXME: For iterative testing
-	marshall.SetDaysToCompleteCourse(marshallDays); //FIXME: For iterative testing
-	marshall.SetDegreeProgram(SOFTWARE); //FIXME: For iterative testing
-
-	cout << marshall.GetStudentId() << endl; //FIXME: For iterative testing
-	cout << marshall.GetFirstName() << endl; //FIXME: For iterative testing
-	cout << marshall.GetLastName() << endl; //FIXME: For iterative testing
-	cout << marshall.GetEmailAddress() << endl; //FIXME: For iterative testing
-	cout << marshall.GetAge() << endl; //FIXME: For iterative testing
-
-	//Loop to print each value in array marshall.daysToCompleteCourse
-	for (int i = 0; i < Student::SIZE; ++i) {
-		cout << marshall.GetDaysToCompleteCourse()[i] << " "; 
-		if (i == Student::SIZE - 1) { //New line at the end of the array for readability
-			cout << endl;
-		}
-	}
-	cout << marshall.GetDegreeProgram() << endl; //FIXME: For iterative testing
-
-	marshall.Print(); //FIXME: For iterative testing
-
-
+	
 
 	return 0;
 }
--- roster.cpp
+++ roster.cpp
@@ -1,12 +1,20 @@
 #include "roster.h"
 
-
-void Roster::parse() {
-	int i = 0;
-	for (i = 0; i < numStudents; ++i) {
+//Need to fix so it can grab studentData[] from main.cpp
+/*void Roster::parse() {
+	for (int i = 0; i < numStudents; ++i) {
 		string currentString = studentData[i];
 		vector<string> result;
-		stringstream S_stream()
+		stringstream s_stream(currentString);
+
+		while (s_stream.good()) {
+			string substr;
+			getline(s_stream, substr, ',');
+			result.push_back(substr);
+		}
+		for (int j = 0; j < result.size(); j++) {
+			cout << result.at(j) << endl;
+		}
 	}
-}
+}*/
 
