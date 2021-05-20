f32f3444205c2495b0da19aeb85b351eecd18652
MegaMarsh <marshallmelonhead@hotmail.com>
2021-05-19 20:56:32 -0700

    Finished the roster.h file

--- roster.h
+++ roster.h
@@ -7,23 +7,26 @@
 #include "student.h"
 using namespace std;
 
-const int numStudents = 5;
+
 
 class Roster {
 	public:
 		void add(string studentID, string firstName, string lastName,
 		   string emailAddress, int age, int daysInCourse1, int daysInCourse2,
 		   int daysInCourse3, DegreeProgram degreeprogram);
-		void remove(string studentID);
-		void printAll();
-		void printAverageDaysInCourse(string studentID);
+		void remove(string studentID); //Searches array for ID and deletes it 
+		void printAll(); //calls print() from student class for each student object
+		void printAverageDaysInCourse();
+		void printByDegree(DegreeProgram degreeProgram);
 		void printInvalidEmails();
-		void parse();
-		//Roster(); FIXME: Need to define in cpp
-		//~Roster(); FIXME: Need to define in cpp
+		void parse(string studentRow);
+
+		~Roster(); 
 
 	private:
 		Student * classRosterArray[5];
+		const int numStudents = 5;
+		int finalIndex = -1;
 		
 	
 
